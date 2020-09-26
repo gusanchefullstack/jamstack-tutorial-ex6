f5625978257f5db23c8269eaa9f0b667882a3e3c
Gustavo Sanchez Galarza <gustavosanchezgalarza@gmail.com>
2020-09-26 11:00:41 -0500

    Logging weather API Response

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -5,6 +5,7 @@ exports.handler = async function(event, context, callback) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
 
   const response = await axios.get(url);
+  console.log("Weather API Response:", response);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(response.data)
