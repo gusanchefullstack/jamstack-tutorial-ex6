227ef15dfca843384194938379e1ee29f3c87772
Gustavo Sanchez Galarza <gustavosanchezgalarza@gmail.com>
2020-09-26 10:16:53 -0500

    Add netlify functions

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
 
+  
+
   eleventyConfig.addPassthroughCopy("src/js");
   return {
     dir: {
---
+++ src/js/weather.js
@@ -0,0 +1,37 @@
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  const url = `/.netlify/functions/weatherapi?lat=${crd.latitude}&lon=${crd.longitude}&units=metric`;
+
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then( (data)=> {
+      document.querySelector("#city").textContent = data.name;
+      document.querySelector("#temp").textContent = data.main.temp;
+      document.querySelector("#main").textContent = data.weather[0].main;
+      document.querySelector("#desc").textContent = data.weather[0].description;
+
+      document.querySelector("#weather").classList.remove("hidden");
+    });
+
+  }
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error); //options es el tercer parametro
+
+}
+
+
